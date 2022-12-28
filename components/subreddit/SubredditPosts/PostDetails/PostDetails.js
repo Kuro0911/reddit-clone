87634ddc957f0c125b3a20718cf44d5207e734bf
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-12-29 03:08:52 +0530

    changes to postdetails

--- components/subreddit/SubredditPosts/PostDetails/PostDetails.js
+++ components/subreddit/SubredditPosts/PostDetails/PostDetails.js
@@ -1,46 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import { PostWrapper } from "./PostDetails.style";
 import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
 import RedeemIcon from "@mui/icons-material/Redeem";
 import ShareIcon from "@mui/icons-material/Share";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import { Button, IconButton } from "@mui/material";
+import ForwardOutlinedIcon from "@mui/icons-material/ForwardOutlined";
+import ForwardIcon from "@mui/icons-material/Forward";
 export const PostDetails = ({ details }) => {
-  console.log(details);
-  let x = {
-    data: "this is the second post on the first sub reddit",
-    date: "2022-12-26T20:15:16.074Z",
-    downVotes: "0",
-    postedBy: "dhananajai",
-    sub_id: "first",
-    title: "this is the second Post",
-    upVotes: "0",
+  const [active, setActive] = useState("");
+  const [votes, setVotes] = useState(Number(details.upVotes));
+  const handleClick = (flag) => {
+    if (flag === "up" && active !== "up") {
+      setVotes(votes + 1);
+      setActive("up");
+    } else if (flag === "up" && active === "up") {
+      setVotes(votes - 1);
+      setActive("");
+    } else if (flag === "down" && active !== "down") {
+      setVotes(votes - 1);
+      setActive("down");
+    } else {
+      setVotes(votes + 1);
+      setActive("");
+    }
   };
   return (
     <PostWrapper>
-      <h1 className="title">{details.title}</h1>
-      <span className="posted">{`u/${details.postedBy}`}</span>
-      <hr />
-      <div className="body">
-        <p>{details.data}</p>
+      <div className="vote">
+        {active === "up" ? (
+          <ForwardIcon
+            className="upvote-act"
+            onClick={() => {
+              handleClick("up");
+            }}
+          />
+        ) : (
+          <ForwardOutlinedIcon
+            className="upvote"
+            onClick={() => {
+              handleClick("up");
+            }}
+          />
+        )}
+        <div className="number">{details.upVotes}</div>
+        {active === "down" ? (
+          <ForwardIcon
+            className="downvote-act"
+            onClick={() => {
+              handleClick("down");
+            }}
+          />
+        ) : (
+          <ForwardOutlinedIcon
+            className="downvote"
+            onClick={() => {
+              handleClick("down");
+            }}
+          />
+        )}
       </div>
-      <div className="footer">
-        <Button
-          variant="text"
-          startIcon={<ChatBubbleOutlineIcon />}
-          className="btn"
-        >
-          <span>Comments</span>
-        </Button>
-        <Button variant="text" startIcon={<RedeemIcon />} className="btn">
-          <span>Award</span>
-        </Button>
-        <Button variant="text" startIcon={<ShareIcon />} className="btn">
-          <span>Share</span>
-        </Button>
-        <IconButton className="btn">
-          <MoreHorizIcon />
-        </IconButton>
+      <div className="right">
+        <h1 className="title">{details.title}</h1>
+        <span className="posted">{`u/${details.postedBy}`}</span>
+        <hr />
+        <div className="body">
+          <p>{details.data}</p>
+        </div>
+        <div className="footer">
+          <Button
+            variant="text"
+            startIcon={<ChatBubbleOutlineIcon />}
+            className="btn"
+          >
+            <span>Comments</span>
+          </Button>
+          <Button variant="text" startIcon={<RedeemIcon />} className="btn">
+            <span>Award</span>
+          </Button>
+          <Button variant="text" startIcon={<ShareIcon />} className="btn">
+            <span>Share</span>
+          </Button>
+          <IconButton className="btn">
+            <MoreHorizIcon />
+          </IconButton>
+        </div>
       </div>
     </PostWrapper>
   );
--- components/subreddit/SubredditPosts/PostDetails/PostDetails.style.js
+++ components/subreddit/SubredditPosts/PostDetails/PostDetails.style.js
@@ -1,13 +1,64 @@
 import styled from "styled-components";
 
 export const PostWrapper = styled.div`
+  display: flex;
   height: 100vh;
   background-color: white;
   width: 60%;
   height: 100%;
   margin-bottom: 0.75em;
   border-radius: 5px 5px 5px 5px;
-  padding: 1em;
+    .vote {
+      display: flex;
+      flex-direction: column; 
+      align-items: center;
+      height: 100%
+      width: 5%;
+      border-radius: 5px 0px 0px 5px;
+      background-color: #d7d8d9;
+      .number{
+      font-size: 12px;
+      font-weight: 700;
+      margin-bottom: 0.25em;
+      color: ${(props) =>
+        props.active === "up"
+          ? "#ff6a33"
+          : props.active === "down"
+          ? "#7193ff"
+          : "black"};
+      }
+      .upvote{      
+        transform: rotateZ(270deg);
+        color: #7c7c7c;
+        cursor: pointer;
+      }
+      .upvote-act{      
+        transform: rotateZ(270deg);
+        color: #ff6a33;
+        cursor: pointer;
+      }
+      .upvote:hover{
+        color: #ff6a33;
+      }
+      .downvote{
+        transform: rotateZ(90deg);
+        color: #7c7c7c;
+        cursor: pointer;
+      }
+      .downvote-act{      
+        transform: rotateZ(90deg);
+        color: #7193ff;
+        cursor: pointer;
+      }
+      .downvote:hover{
+        color: #7193ff;
+      }
+    }
+
+  .right{
+    padding: 0.25em;
+    margin-left: 1em
+  }
   .title {
     font-size: 1.5em;
     text-transform: capitalize;
